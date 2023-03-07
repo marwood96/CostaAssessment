be380dfc7d5ca35616e97c380847053ee322da16
marwood96 <119493413+marwood96@users.noreply.github.com>
2023-03-07 16:29:21 +0000

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,250 @@
+function myFunction() {
+    var OurCoffeesPage = document.getElementById("OurCoffeesPage");
+    if (OurCoffeesPage.style.display === "none") {
+        OurCoffeesPage.style.display = "block";
+        HomePage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        OrderOnlinePage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = true;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        OurCoffeesPage.style.display = "none";
+    }
+  }
+
+
+
+function myFunction2() {
+    var OurMenuPage = document.getElementById("OurMenuPage");
+    if (OurMenuPage.style.display === "none") {
+        OurMenuPage.style.display = "block";
+        HomePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OrderOnlinePage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = true;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        OurMenuPage.style.display = "none";
+    }
+  }
+
+function myFunction3() {
+    var CostaClubPage = document.getElementById("CostaClubPage");
+    if (CostaClubPage.style.display === "none") {
+        CostaClubPage.style.display = "block";
+        HomePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        OrderOnlinePage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = true;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        CostaClubPage.style.display = "none";
+    }
+  }
+
+function myFunction4() {
+    var OrderOnlinePage = document.getElementById("OrderOnlinePage");
+    if (OrderOnlinePage.style.display === "none") {
+        OrderOnlinePage.style.display = "block";
+        HomePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = true;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        OrderOnlinePage.style.display = "none";
+    }
+  }
+
+function myFunction5() {
+    var SustainabilityPage = document.getElementById("SustainabilityPage");
+    if (SustainabilityPage.style.display === "none") {
+        SustainabilityPage.style.display = "block";
+        HomePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        OrderOnlinePage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("sustainability").disabled = true;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        SustainabilityPage.style.display = "none";
+    }
+  }
+
+
+function myFunction6() {
+    var HomePage = document.getElementById("HomePage");
+    if (HomePage.style.display === "none") {
+        HomePage.style.display = "block";
+        OrderOnlinePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = true;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        OrderOnlinePage.style.display = "none";
+    }
+  }
+
+
+function myFunction7() {
+    var PrivacyPolicyPage = document.getElementById("PrivacyPolicyPage");
+    if (PrivacyPolicyPage.style.display === "none") {
+        PrivacyPolicyPage.style.display = "block";
+        OrderOnlinePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        HomePage.style.display = "none";
+        CookiePage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = true;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        PrivacyPolicyPage.style.display = "none";
+    }
+  }
+
+
+function myFunction8() {
+    var CookiePage = document.getElementById("CookiePage");
+    if (CookiePage.style.display === "none") {
+        CookiePage.style.display = "block";
+        OrderOnlinePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        HomePage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        TermsPage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = true;
+        document.getElementById("termsbutton").disabled = false;
+    } else {
+        CookiePage.style.display = "none";
+    }
+  }
+
+
+function myFunction9() {
+    var TermsPage = document.getElementById("TermsPage");
+    if (TermsPage.style.display === "none") {
+        TermsPage.style.display = "block";
+        OrderOnlinePage.style.display = "none";
+        OurCoffeesPage.style.display = "none";
+        OurMenuPage.style.display = "none";
+        CostaClubPage.style.display = "none";
+        SustainabilityPage.style.display = "none";
+        HomePage.style.display = "none";
+        PrivacyPolicyPage.style.display = "none";
+        CookiePage.style.display = "none";
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("ourMenu").disabled = false;
+        document.getElementById("costaClub").disabled = false;
+        document.getElementById("orderOnline").disabled = false;
+        document.getElementById("ourCoffees").disabled = false;
+        document.getElementById("logoclick").disabled = false;
+        document.getElementById("sustainability").disabled = false;
+        document.getElementById("privacybutton").disabled = false;
+        document.getElementById("cookiebutton").disabled = false;
+        document.getElementById("termsbutton").disabled = true;
+
+    } else {
+        TermsPage.style.display = "none";
+    }
+  }
+
+
